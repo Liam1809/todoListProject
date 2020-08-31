3eff084681f7c054a3d98797dd82d618f0e086b7
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-08-31 11:14:43 +0700

    Event Bubbling concept: nested child element triggers, its parent also triggers

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,16 +1,20 @@
 // Check Off Specific Todos By Clicking
 $("li").on("click", function(){
-	if($(this).css("color") === "rgb(128, 128, 128)") {
-		$(this).css({
-			color: "black",
-			textDecoration: "none"
-		});
-	}
-	else {
-		$(this).css({
-			color: "gray",
-			textDecoration: "line-through"
-		});
-	}
+	$(this).toggleClass("strike-through");
 });
-	
+// Click on X to delete todo
+$("span").on("click", function(){
+	alert("you click on span");
+});
+
+$("li").on("click", function(){
+	alert("you click on li");
+});
+
+$("body").on("click", function(){
+	alert("you click on body");
+});
+
+$("html").on("click", function(){
+	alert("you click on html");
+});
