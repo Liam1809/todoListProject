996352f187529201694198c08c0459cbe08a4ce4
Liam Ha <50177344+Liam1809@users.noreply.github.com>
2020-08-31 22:41:42 +0700

    creating Todos, adding on(clicl) for potential future lis and spans

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,14 +1,26 @@
 // Check Off Specific Todos By Clicking
-$("li").on("click", function(){
+$("ul").on("click", "li", function(){
 	$(this).toggleClass("strike-through");
 });
 // Click on X to delete todo
-$("span").on("click", function(event){
+$("ul").on("click", "span", function(event){
 	// remove
 	$(this).parent().fadeOut(500, function(){
 		$(this).remove();
 	});
 	// Stop the event Bubbling from other elements
 	event.stopPropagation();
+});
+
+// Creating Todos
+$('input[type="text"]').on("keypress", function(event){
+	let text;
+	if(event.which === 13){
+		// grab new todo text from input
+		text = $(this).val();
+		$(this).val("");
+		// create new li and add to ul
+		$("ul").append("<li><span>X</span> " + text + "</li>");
+	}
 
 });
